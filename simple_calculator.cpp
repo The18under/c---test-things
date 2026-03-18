24a6b48eb489629ff964b2e9597bd41ab675276e
the18under <the18under@gmail.com>
2026-03-18 18:16:42 +0100

    added a calculator

---
+++ simple_calculator.cpp
@@ -0,0 +1,46 @@
+#include <iostream> 
+int main () {
+
+double a;
+double b;
+double result;
+char operation;
+
+std::cout << "what do you want 'a' to be\n";
+std::cin >> a;
+
+std::cout << "what do you want 'b' to be\n";
+std::cin >> b;
+
+std::cout << "Do you want to, add or subtract (A for add, S for subtract, M  for muiltplcation, D for divide)\n";
+std::cin >> operation;
+
+if(operation == 'A') { 
+    result = a + b;
+    std::cout <<"the answer is " << result << "\n";
+    return 0;
+}
+
+else if(operation == 'S') { 
+    result = a - b;
+    std::cout <<"the answer is " << result << "\n";
+    return 0;}
+
+    else if(operation == 'M') { 
+    result = a * b;
+    std::cout <<"the answer is " << result << "\n";
+    return 0;
+}
+else if (operation == 'D') {
+    result = a/b;
+    std::cout <<"the answer is " << result << "\n";
+    return 0;
+
+}
+return 0;
+
+}
+
+
+
+
