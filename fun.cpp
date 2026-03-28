9e7c16af12bf9fcd3b37e867683d6c7c931b1729
the18under <the18under@gmail.com>
2026-03-28 08:25:02 +0100

    vectors in loops sucks :[

---
+++ fun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+int main() {
+
+std::vector<int> distance(4);
+int total;
+
+std::cout << "what are your 4 distances in miles\n";
+std::cin >> distance[0];
+std::cin >> distance[1];
+std::cin >> distance[2];
+std::cin >> distance[3];
+
+total = distance[0] + distance[1] + distance[2] + distance[3];
+
+std::cout << "the total miles covered is " << total << " miles\n";
+return 0;
+}
