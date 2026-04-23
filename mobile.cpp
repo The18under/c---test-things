e0e980a505f70ecf4c2fb92bd5ec224bcf810428
The18under <the18under@gmail.com>
2026-04-23 09:46:14 +0100

    Last Sync: 2026-04-23 09:46 (Mobile)

--- mobile.cpp
+++ mobile.cpp
@@ -3,7 +3,11 @@ int main()
 {
 	double x,y,z;
 	double v;
+	char dim;
+	std::cout << "3D(a) or 2D(b)\n";
+	std::cin >> dim;
 	
+	if(dim == 'a') {
 	std::cout << "what do you want the X axis to be?\n";
 	std::cin >> x;
 	
@@ -16,6 +20,20 @@ int main()
 	v = x * y * z;
 	
 	std::cout << "the volume is " << v << "cm³\n";
+	}
 	
-	return 0;
+	else if(dim == 'b'){
+		std::cout << "what do you want the X axis to be?\n";
+	std::cin >> x;
+	
+	std::cout << "what do you want the Y axis to be?\n";
+	std::cin >> y;
+	
+	v = x * y;
+	std::cout << "the volume is " << v << "cm²\n";
+}
+else{
+	std::cout << "invaild\n";
+}
+return 0;
 }
